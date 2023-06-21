7ecacd63b9cdaeaa0a7dff3e74e8bfa97be3e2c3
Barrca <60709314+FranciscoBarCa@users.noreply.github.com>
2023-06-21 18:06:12 +0200

    Updated project config for better imports and gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      src: "/src",
+      atoms: "/src/atoms",
+    },
+  },
+  base: '/RosGen_v2/',
   server: {
     host: true
   }
