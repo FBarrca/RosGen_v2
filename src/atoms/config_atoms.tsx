d75aa008ce046d91e26ebde549bd2258cce5611e
unknown <201800222@alu.comillas.edu>
2023-06-25 23:06:10 +0200

    Added some documentation to config atoms

--- src/atoms/config_atoms.tsx
+++ src/atoms/config_atoms.tsx
@@ -7,51 +7,50 @@ export interface CanvasStateInterface {
   position: { x: number; y: number };
 }
 
+// Canvas position for panning and zooming
 export const canvasStateAtom = atomWithReset<CanvasStateInterface>({
   scale: 1,
   position: { x: 0, y: 0 },
 });
 
+// Stores the Konva.Stage instance mainly so we can acess Konva nodes through .find()
 export const stageAtom = atomWithReset<Konva.Stage | null>(null);
 
+// Stores the tool from Sidebar that is currently active
 type ActiveTool = "comment" | "connection" | "node" | "topic" | "service";
-
 export const activeToolAtom = atom<ActiveTool>("node");
 
-
+// Used for the connection tool, stores the id of the node or topic that is currently selected
 interface connectionAtomInterface {
   node: string | null;
   topic: string | null;
 }
-
 export const connectionAtom = atomWithReset<connectionAtomInterface>({
   node: null,
   topic: null,
 });
 
 export const addNodeConnectionAtom = atom(null, (get, set, nodeID: string) => {
-  
   set(connectionAtom, (prev) => ({ ...prev, node: nodeID }));
   const connection = get(connectionAtom);
   if (connection.topic == null) return false;
   return connection.topic;
 });
 
-
-export const addTopicConnectionAtom = atom(null, (get, set, topicID: string) => {
-  set(connectionAtom, (prev) => ({ ...prev, topic: topicID }));
-  const connection = get(connectionAtom);
-  if (connection.node == null) return false;
-  return connection.node;
-});
-
+export const addTopicConnectionAtom = atom(
+  null,
+  (get, set, topicID: string) => {
+    set(connectionAtom, (prev) => ({ ...prev, topic: topicID }));
+    const connection = get(connectionAtom);
+    if (connection.node == null) return false;
+    return connection.node;
+  }
+);
 
 //  force update
 
-export const forceUpdateAtom = atom<boolean>(false)
+export const forceUpdateAtom = atom<boolean>(false);
 
 export const toggleForceUpdateAtom = atom(null, (get, set) => {
   set(forceUpdateAtom, (prev) => !prev);
-}
-)
-
+});
